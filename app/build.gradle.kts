7539e9f9c2977276c97da57ea034623263231a88
JakwanAja <muhzakwanalfaris195@gmail.com>
2025-05-22 00:02:21 +0700

    Add drawable resource, MainActivity,Login&RegisterActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,40 +26,46 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
+    implementation("androidx.core:core:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+// Lifecycle
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    annotationProcessor("androidx.lifecycle:lifecycle-compiler:2.6.2")
 
-// Lifecycle components
-    implementation(libs.androidx.lifecycle.livedata)
-    implementation(libs.androidx.lifecycle.viewmodel)
-    implementation(libs.androidx.lifecycle.extensions)
-    annotationProcessor(libs.androidx.lifecycle.compiler)
-
-// Room Database
-    implementation(libs.androidx.room.rxjava3)
-    implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.compiler)
+// Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-rxjava3:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
 
 // Glide
-    implementation(libs.glide)
-    annotationProcessor(libs.glide.compiler)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
 // RxJava
-    implementation(libs.rxjava3.android)
-    implementation(libs.rxjava3.core)
+    implementation("io.reactivex.rxjava3:rxjava:3.1.8")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+
+    // Unit test
+    testImplementation("junit:junit:4.13.2")
+
+    // Instrumentation test
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
